cb01889df79607f3f961c5f31332600506f22de7
yanglei <715860993@qq.com>
2025-12-18 10:21:35 +0800

    添加图片选择和预览功能，更新权限管理，重构相关组件以支持相机和图片选择器

--- core/src/main/java/reksai/compose/core/component/image/MyImage.kt
+++ core/src/main/java/reksai/compose/core/component/image/MyImage.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -71,8 +72,8 @@ fun MyImage(
 private fun MyImageContentPreview() {
     Column(
         modifier = Modifier
-            .fillMaxSize()
             .background(LocalColors.current.background)
+            .padding(16.dp)
     ) {
         Box(
             modifier = Modifier
