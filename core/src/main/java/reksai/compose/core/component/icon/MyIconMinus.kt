3ba6422bdea70cb540c000b92718d6ad55e93073
yanglei <715860993@qq.com>
2026-01-01 22:05:09 +0800

    添加 MyIconAdd 和 MyIconMinus 组件，支持自定义图标渲染

---
+++ core/src/main/java/reksai/compose/core/component/icon/MyIconMinus.kt
@@ -0,0 +1,27 @@
+package reksai.compose.core.component.icon
+
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.unit.dp
+import reksai.compose.core.R
+import reksai.compose.core.theme.LocalColors
+
+@Composable
+fun MyIconMinus(
+    modifier: Modifier = Modifier,
+    tint: Color = LocalColors.current.black200,
+) {
+    Icon(
+        imageVector = ImageVector.vectorResource(R.drawable.icon_minus),
+        contentDescription = null,
+        tint = tint,
+        modifier = Modifier
+            .then(modifier)
+            .size(24.dp)
+    )
+}
