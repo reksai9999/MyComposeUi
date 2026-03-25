5fd329706315aa119f8444d127ca0cacb3b0a1f8
yanglei <715860993@qq.com>
2026-03-25 12:44:01 +0800

    更新权限配置，添加外部存储读取权限，优化 ImageScreen 布局，升级依赖版本

--- app/src/main/java/reksai/compose/ui/ui/screen/ImageScreen.kt
+++ app/src/main/java/reksai/compose/ui/ui/screen/ImageScreen.kt
@@ -4,10 +4,14 @@ import android.net.Uri
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -52,6 +56,7 @@ fun ImageScreen(
                 .weight(1f)
                 .background(LocalColors.current.background)
                 .padding(16.dp)
+                .verticalScroll(rememberScrollState())
         ) {
 
             Text(
@@ -128,6 +133,8 @@ fun ImageScreen(
                     modifier = Modifier.size(120.dp, 150.dp)
                 )
             }
+
+            Spacer(modifier = Modifier.height(200.dp))
 
 
         }
