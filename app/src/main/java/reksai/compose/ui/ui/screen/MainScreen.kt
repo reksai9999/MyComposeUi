c60d604705d8be263d2bf81cd896724f42fa89ad
yanglei <715860993@qq.com>
2026-04-14 08:53:15 +0800

    添加 SuperText 组件及其路由，更新 MainScreen 和 Navigation 以支持新功能

--- app/src/main/java/reksai/compose/ui/ui/screen/MainScreen.kt
+++ app/src/main/java/reksai/compose/ui/ui/screen/MainScreen.kt
@@ -2,8 +2,10 @@ package reksai.compose.ui.ui.screen
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
@@ -113,6 +115,7 @@ fun MainScreen(
                 MyRoute.add(RouteBadge)
             }
 
+            Spacer(modifier = Modifier.height(200.dp))
 
         }
     }
