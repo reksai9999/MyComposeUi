3fc30f0eb007af4938f64740ab1b8fd8259e874b
yanglei <715860993@qq.com>
2026-08-19 14:23:06 +0800

    添加字符串转换功能，新增 toLongOrZero 方法以处理转换失败的情况

--- core/src/main/java/reksai/compose/core/component/selector/MyDropdown.kt
+++ core/src/main/java/reksai/compose/core/component/selector/MyDropdown.kt
@@ -1,5 +1,6 @@
 package reksai.compose.core.component.selector
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
@@ -8,14 +9,19 @@ import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.MenuDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.Density
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.DpOffset
 import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.IntRect
@@ -34,6 +40,11 @@ fun <T> MyDropdown(
     onItemSelected: (T) -> Unit,
     dropdownModifier: Modifier = Modifier,
     offset: DpOffset = DpOffset(0.dp, 0.dp),
+    shape: Shape = MaterialTheme.shapes.extraSmall,
+    containerColor: Color = MaterialTheme.colorScheme.surface,
+    tonalElevation: Dp = MenuDefaults.TonalElevation,
+    shadowElevation: Dp = MenuDefaults.ShadowElevation,
+    border: BorderStroke? = null,
     properties: PopupProperties = PopupProperties(focusable = true),
     anchorContent: @Composable (selectedItem: T?, isExpanded: Boolean) -> Unit,
     itemContent: @Composable (item: T, isSelected: Boolean) -> Unit
